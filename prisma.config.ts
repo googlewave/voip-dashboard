221587d66e38ba133e1b3c6760aa8889c267ecb9
Bob Liu <bob@Bobs-MacBook-Air.local>
2026-03-08 21:59:20 -0400

    Remove datasource from prisma.config.ts

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,10 +1,6 @@
-import 'dotenv/config';
-import { defineConfig, env } from 'prisma/config';
+import { defineConfig } from 'prisma/config';
 
 export default defineConfig({
   earlyAccess: true,
   schema: 'prisma/schema.prisma',
-  datasource: {
-    url: env('DATABASE_URL'),
-  },
 });
