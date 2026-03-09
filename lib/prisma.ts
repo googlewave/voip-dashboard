21f6d133d06164ed4bfb60bc73cdb30af493ac4c
Bob Liu <bob@Bobs-MacBook-Air.local>
2026-03-08 22:43:22 -0400

    Simplify PrismaClient init

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -3,13 +3,7 @@ import { PrismaClient } from '@prisma/client';
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
 function createPrismaClient() {
-  return new PrismaClient({
-    datasources: {
-      db: {
-        url: process.env.DATABASE_URL,
-      },
-    },
-  });
+  return new PrismaClient();
 }
 
 export const prisma = globalForPrisma.prisma || createPrismaClient();
